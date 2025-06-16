c3b4c325a485420d8f39aa797415ff0e4ac89907
kabasawa <sthyt915@yahoo.ne.jp>
2025-06-16 22:43:02 +0900

    Initial commit

---
+++ src/test/java/com/example/learning_progress/LearningProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learning_progress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningProgressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
